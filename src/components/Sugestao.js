c8bdc4be918a5435aa2fceeb59c728562977805d
AiKurogaya <yujikuroda12@hotmail.com>
2022-07-18 14:47:50 -0300

    Testing a lot of .js

---
+++ src/components/Sugestao.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Sugestao() {
+    const sugestoes = [
+        {imagem: "/assets/img/bad.vibes.memes.svg", nomeTexto: "bad.vibes.memes", razaoTexto: "Segue você"},
+        {imagem: "/assets/img/chibirdart.svg", nomeTexto: "chibirdart", razaoTexto: "Segue você"},
+        {imagem: "/assets/img/razoesparaacreditar.svg", nomeTexto: "razoesparaacreditar", razaoTexto: "Novo no Instagram"},
+        {imagem: "/assets/img/adorable_animals.svg", nomeTexto: "adorable_animals", razaoTexto: "Segue você"},
+        {imagem: "/assets/img/smallcutecats.svg", nomeTexto: "smallcutecats", razaoTexto: "Segue você"}
+    ];
+
+    return (
+        <div class="sugestoes">
+
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            {sugestoes.map(sugestao => 
+                <div class="sugestao">
+                    <div class="usuario">
+                        <img src={sugestao.imagem} alt="" />
+                        <div class="texto">
+                            <div class="nome">{sugestao.nomeTexto}</div>
+                            <div class="razao">{sugestao.razaoTexto}</div>
+                        </div>
+                    </div>
+                    <div class="seguir">Seguir</div>
+                </div>
+            )}
+
+        </div>
+    );
+}
---
+++ src/components/Usuario.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Usuario() {
+    return (
+        <div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+                <strong>catanacomics</strong>
+                Catana
+            </div>
+        </div>
+    );
+}
